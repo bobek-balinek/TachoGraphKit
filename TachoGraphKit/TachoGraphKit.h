d83f7bad2e434732acc2180fa54e6fba2913a4c0
Przemyslaw Bobak <bobak.art@gmail.com>
2017-04-10 21:31:18 +0100

    Initial Commit

---
+++ TachoGraphKit/TachoGraphKit.h
@@ -0,0 +1,19 @@
+//
+//  TachoGraphKit.h
+//  TachoGraphKit
+//
+//  Created by Przemyslaw Bobak on 10/04/2017.
+//  Copyright © 2017 Spirograph Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TachoGraphKit.
+FOUNDATION_EXPORT double TachoGraphKitVersionNumber;
+
+//! Project version string for TachoGraphKit.
+FOUNDATION_EXPORT const unsigned char TachoGraphKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TachoGraphKit/PublicHeader.h>
+
+
